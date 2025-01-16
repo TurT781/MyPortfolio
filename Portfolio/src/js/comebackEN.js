7a249b0225a28856a2bc246eebf0f720ab4007ce
Flavien Ptrk <flavien.patriarca2002@gmail.com>
2025-01-16 14:05:22 +0100

    refactor(me) : delete useless files

--- Portfolio/src/js/comebackEN.js
+++
@@ -1,8 +0,0 @@
-let docTitle = document.title;
-window.addEventListener("blur", () => {
-    document.title = "Come back 😞";
-})
-window.addEventListener('focus', () => {
-    document.title = docTitle;
-})
-
