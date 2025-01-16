7691ed61f7b9ea3d0be71678bc5b5a99f3966907
Flavien Ptrk <flavien.patriarca2002@gmail.com>
2025-01-16 10:17:43 +0100

    feat : dev tool enabled

--- Portfolio/src/js/comeback.js
+++ Portfolio/src/js/comeback.js
@@ -6,20 +6,20 @@ window.addEventListener("focus", () => {
     document.title = docTitle;
 });
 
-// (function () {
-//     const detectDevTools = function () {
-//         const widthThreshold = 100; // Get the width difference (means DevTools is opened)
+(function () {
+    const detectDevTools = function () {
+        const widthThreshold = 100; // Get the width difference (means DevTools is opened)
 
-//         if (window.outerWidth - window.innerWidth > widthThreshold ||
-//             window.outerHeight - window.innerHeight > widthThreshold) {
+        if (window.outerWidth - window.innerWidth > widthThreshold ||
+            window.outerHeight - window.innerHeight > widthThreshold) {
             
-//             document.body.innerHTML = "<h1>Accès refusé <br> </h1><p>Veuillez fermer les outils de développement pour continuer. <br> <br> Rafraichissez ensuite la page.</p>";
+            document.body.innerHTML = "<h1>Accès refusé <br> </h1><p>Veuillez fermer les outils de développement pour continuer. <br> <br> Rafraichissez ensuite la page.</p>";
                 
-//             setInterval(() => {
-//                 document.body.innerHTML = "<h1>Accès refusé  <br> </h1><p>Veuillez fermer les outils de développement pour continuer. <br> <br> Rafraichissez ensuite la page.</p>";
-//             }, 100);
-//         }
-//     };
-//     // N ms
-//     setInterval(detectDevTools, 100);
-// })();
+            setInterval(() => {
+                document.body.innerHTML = "<h1>Accès refusé  <br> </h1><p>Veuillez fermer les outils de développement pour continuer. <br> <br> Rafraichissez ensuite la page.</p>";
+            }, 100);
+        }
+    };
+    // N ms
+    setInterval(detectDevTools, 100);
+})();
